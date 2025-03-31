351ece55bcb68811c2ebed777feee7c34d496b0c
Madhav M. <madhavmanoj117@gmail.com>
2025-03-31 23:30:28 +0100

    fix: add category routes to router

--- server/wonki_waste_server/api/routes/__init__.py
+++ server/wonki_waste_server/api/routes/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
-from wonki_waste_server.api.routes import auth
+from wonki_waste_server.api.routes import auth, category, food_waste
 
 api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(auth.router)
+api_router.include_router(category.router)
+api_router.include_router(food_waste.router)
